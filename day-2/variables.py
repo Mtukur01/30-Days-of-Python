e50ca1b53e90cfe1e42b56a4bbf0832c1f6adab8
Mtukur01 <117780885+Mtukur01@users.noreply.github.com>
2025-07-21 11:51:48 +0100

    variables.py

--- day-2/variables.py
+++ day-2/variables.py
@@ -1,2 +1,24 @@
-First_name = Mustapha
-Second_name = Tukur
+#Day 2: 30 Days of python programming
+first_name = Mustapha
+second_name = Tukur
+full_name = Mustapha Tukur
+country = Nigeria
+city = Funtua
+age = 25
+year = 2025
+is_married = No
+is_true = True
+is_light_on = Yes
+x,y,z = 10,'rate',6.3
+
+print(type(first_name))
+print(type(second_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x,y,x))
